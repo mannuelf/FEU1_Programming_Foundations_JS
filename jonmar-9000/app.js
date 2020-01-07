168ad60f573bf14a57a37759dfa4b19735f531cd
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-07 14:42:59 +0100

    first-commit

--- jonmar-9000/app.js
+++ jonmar-9000/app.js
@@ -1,33 +1,29 @@
-var isAlive = true;
-var name = "Jonmar-9000";
-var hasJetPack = true;
-var suitColor = "rosa";
-var eyes = 2;
-var eyeColor = "svart";
-var hairColor = "black";
-var legs = 2;
-var arms = 2;
-var hands = 2;
-var ears = 2;
-var canWalk = true;
-var canRun = true;
-var canFly = true;
-var hasWeapon = true;
-var isInGoodMood = true;
+const droid = {
+  isAlive: true,
+  name: "Jonmar-9000",
+  hasJetPack: true,
+  suitColor: "rosa",
+  eyes: 2,
+  eyeColor: "svart",
+  hairColor: "black",
+  legs: 2,
+  arms: 2,
+  hands: 2,
+  ears: 2,
+  canWalk: true,
+  canRun: true,
+  canFly: true,
+  hasWeapon: true,
+  isInGoodMood: true
+};
+
+if (droid.hasJetPack) {
+  console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+} else {
+  console.log(
+    "JetPack NOT enabled, please enable jetPack before doing a space walk"
+  );
+}
 
 console.log(".-.-. Booting .-.-.");
-console.log(
-  name,
-  isAlive,
-  hasJetPack,
-  eyes,
-  eyeColor,
-  hairColor,
-  legs,
-  arms,
-  hands,
-  ears,
-  canWalk,
-  canFly,
-  hasWeapon
-);
+console.log(droid);
