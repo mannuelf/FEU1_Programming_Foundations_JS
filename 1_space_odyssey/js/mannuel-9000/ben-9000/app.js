5341523048642e2aba390cc49ef612d39b2bc3e7
Ben Noah <noahbenaas@gmail.com>
2020-01-07 14:21:32 +0100

    update js

---
+++ 1_space_odyssey/js/mannuel-9000/ben-9000/app.js
@@ -0,0 +1,19 @@
+var isAlive = true; 
+var name = "ben-9000";
+var hasJetPack = true;
+var suitColor = "black";
+var eyes = 4;
+var eyeColor = "brown";
+var hairColor = 0;
+var legs = 2 ;
+var arms = 2;
+var hands = 2;
+var ears = 2;
+var canWalk = true;
+var canRun = true;
+var canFly = true;
+var hasWeapon = false;
+var isInGoodMood = false;
+
+console.log(".-.-. Booting .-.-.");
+console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
