edb6b365d4f16ad02e335cbc6d63fab14ddd01dd
Trond <56018387+tronderiklarsen@users.noreply.github.com>
2020-01-07 14:27:23 +0100

    //1 & //2
    Defined variables and added if/else if statements

---
+++ 1_space_odyssey/js/trond-9000/app.js
@@ -0,0 +1,32 @@
+//1
+
+var isAlive = true;
+var name = "Trond-9000";
+var hasJetPack = true;
+var suitColor = "white";
+var eyes = true;
+var eyeColor = "blue";
+var hairColor ="dark blonde";
+var legs = 2;
+var arms = 2;
+var hands = 2; 
+var ears = 2;
+var canWalk = true;
+var canRun = true;
+var canFly = true;
+var hasWeapon = true;
+var isInGoodMood = true;
+
+console.log(".-.-. Booting .-.-.");
+console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
+
+//2
+
+if (hasJetPack === true) {
+    console.log("Droid can go outside");
+}
+else if (hasJetPack === false) {
+    console.log("Droid can't go outside");
+}
+
+//3
