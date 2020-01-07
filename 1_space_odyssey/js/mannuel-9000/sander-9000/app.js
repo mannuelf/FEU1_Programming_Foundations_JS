cc9a97901fac02d85832166c876af69256ed8a80
Sander Byrkjeland <sunbyr@gmail.com>
2020-01-07 14:45:55 +0100

    Task 2
    Task 2 complete

--- 1_space_odyssey/js/mannuel-9000/sander-9000/app.js
+++ 1_space_odyssey/js/mannuel-9000/sander-9000/app.js
@@ -106,13 +106,19 @@ console.log(".-.-. Booting .-.-.");
 console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
 
 
-
 /* 
  2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
     a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid 
        cannot go outside, console log the correct log based on that condition. 
 */
+
+if (hasJetPack){
+    console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+} else{
+    console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
+
+}
 
 /* 
  uncomment these console logs and write your if else statement and use them to console log out the correct statement
