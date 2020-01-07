d38c36354df7402b75824f7fdf54f6dafa09a78f
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-01-07 14:22:45 +0100

    I have added my branch

---
+++ 1_space_odyssey/js/mannuel-9000/farida-9000/app.js
@@ -0,0 +1,19 @@
+var isAlive = true;
+var name = "farida-9000";
+var hasJetPack = true;
+var suitColor = "pink";
+var eyes = 2;
+var eyeColor = "green";
+var hairColor = "red";
+var legs = 2;
+var arms = 2;
+var hands = 2;
+var ears = 2;
+var canWalk = true;
+var canRun = true;
+var canFly = true;
+var hasWeapon = true;
+var isInGoodMood = false;
+
+console.log(".-.-. Booting .-.-.");
+console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
