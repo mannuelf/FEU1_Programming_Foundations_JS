fcb537e5b2f4ac86acdea67230b9109aa801cf0c
mathiaskonye <56630416+mathiaskonye@users.noreply.github.com>
2020-01-07 14:38:37 +0100

    mathias-9000
    mathias-9000 humanoid

---
+++ 1_space_odyssey/js/mannuel-9000/mathias-9000/script.js
@@ -0,0 +1,26 @@
+var isAlive = true;
+var name = "mathias-9000";
+var hasJetPack = true;
+var suitColor = "neongreen";
+var eyes = true;
+var eyeColor = "brown";
+var hairColor = "brown";
+var legs = 2;
+var arms = 2;
+var hands = 2;
+var ears = 2;
+var canWalk = true;
+var canRun = false;
+var canFly = true;
+var hasWeapon = true;
+var isInGoodMood = true;
+
+if (hasJetPack){
+    console.log("jetpack is on")
+}
+else{
+    console.log("jetpack is not on")
+}
+
+console.log(".-.-. Booting .-.-.");
+console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
