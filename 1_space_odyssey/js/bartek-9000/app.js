ffddfddb30888860fa2f0db4d048e2f7e8e59eaa
bartektelec <bartektelec@gmail.com>
2020-01-07 14:23:03 +0100

    added bartek-9000

---
+++ 1_space_odyssey/js/bartek-9000/app.js
@@ -0,0 +1,33 @@
+var isAlive = true;
+var name = "Bartek-9000";
+var hasJetPack = true;
+var suitColor = "gold";
+var eyes = 4;
+var eyeColor = "skyblue";
+var hairColor = null;
+var legs = 3;
+var arms = 2;
+var hands = 4;
+var ears = 1;
+var canWalk = true;
+var canRun = true;
+var canFly = true;
+var hasWeapon = true;
+var isInGoodMood = false;
+
+console.log(".-.-. Booting .-.-.");
+console.log(
+  name,
+  isAlive,
+  hasJetPack,
+  eyes,
+  eyeColor,
+  hairColor,
+  legs,
+  arms,
+  hands,
+  ears,
+  canWalk,
+  canFly,
+  hasWeapon
+);
