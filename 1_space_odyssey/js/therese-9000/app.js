6748d170fd20b93d4dcd33fed7d4a23c9d94b132
Therese Lybo <therese.lybo@gmail.com>
2020-01-08 13:44:37 +0100

    Minor aesthetic changes

--- 1_space_odyssey/js/therese-9000/app.js
+++ 1_space_odyssey/js/therese-9000/app.js
@@ -105,8 +105,6 @@ var isInGoodMood = true;
 console.log(".-.-. Booting .-.-.");
 console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
 
-
-
 /* 
  2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
