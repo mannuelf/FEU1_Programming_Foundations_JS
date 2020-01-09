49f8fd20b8954273b7f5b36be1784c33c60bfdc0
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-01-09 09:21:43 +0100

    Indented brackets on the same line

--- 1_space_odyssey/js/farida-9000/app.js
+++ 1_space_odyssey/js/farida-9000/app.js
@@ -21,9 +21,6 @@ console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, ha
 
 if (hasJetPack === true){    
     console.log("JetPack is enabled!!! off I go outside to fix the satellite");
-}
-else {
+} else {
     console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 }
-
-
