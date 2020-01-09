fb55d13c1c29594058f4ec7245bb88f8a7f1ead8
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-09 11:17:47 +0100

    jonmar-meteor-shower

--- 1_space_odyssey/js/jonmar-9000/app.js
+++ 1_space_odyssey/js/jonmar-9000/app.js
@@ -1,3 +1,5 @@
+// 1st Exercise
+
 const droid = {
   name: "Jonmar-9000",
   isAlive: true,
@@ -23,6 +25,8 @@ const droid = {
   }
 };
 
+// 2nd Exercise
+
 droid.hasJetPack
   ? console.log("JetPack is enabled!!! off I go outside to fix the satellite")
   : console.log(
@@ -31,3 +35,44 @@ droid.hasJetPack
 
 console.log(".-.-. Booting .-.-.");
 console.log(droid);
+
+// 3rd Exercise
+
+console.log("\u{1F327} METEOR SHOWER::::::");
+
+var responses = [
+  "Call basecamp and ask for assistance, but they are far away",
+  "Do nothing, just push through",
+  "Initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
+  "Ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
+];
+
+let safetyResponse = "\u{1F6F8} Swoosh -> -> Ship is steered to safety!";
+
+for (var i = 0; i <= responses.length; i++) {
+  if (responses[i] === responses[2]) console.log(safetyResponse);
+}
+
+// 4th Exercise
+
+console.log("\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::");
+
+var enemyCraft = true;
+var photonLaser = true;
+var photonLaserEnabled = true;
+var bullets = 101;
+
+var bulletsNeeded = 100;
+
+var win = "\u{2708} \u{1F525} pew pew pew !! you got them!!";
+var lose = "\u{1F9E8} DEAD \u{1F9E8} ";
+
+if (enemyCraft && photonLaser && photonLaserEnabled) {
+  if (bullets >= bulletsNeeded) {
+    for (var i = 0; i < bulletsNeeded; i++) {
+      console.log(win);
+    }
+  } else {
+    console.log(lose);
+  }
+}
