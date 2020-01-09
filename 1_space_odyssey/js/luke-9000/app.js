d6292c642a452d57f6e6539a879810f97fe5abc6
cvampiri <cvampiri@gmail.com>
2020-01-09 15:06:02 +0100

    Hi here my work
    Here

--- 1_space_odyssey/js/luke-9000/app.js
+++ 1_space_odyssey/js/luke-9000/app.js
@@ -103,19 +103,19 @@ var isInGoodMood = false;
 
 console.log("\u{1F916} .-.-. Booting .-.-.");
 console.log(
-  name,
-  isAlive,
-  hasJetPack,
-  eyes,
-  eyeColor,
-  hairColor,
-  legs,
-  arms,
-  hands,
-  ears,
-  canWalk,
-  canFly,
-  hasWeapon
+    name,
+    isAlive,
+    hasJetPack,
+    eyes,
+    eyeColor,
+    hairColor,
+    legs,
+    arms,
+    hands,
+    ears,
+    canWalk,
+    canFly,
+    hasWeapon
 );
 
 /*
@@ -125,19 +125,19 @@ console.log(
        cannot go outside, console log the correct log based on that condition.
 */
 console.log(
-  "\u{1F6F0} BROKEN SATELLITE::::::"
+    "\u{1F6F0} BROKEN SATELLITE::::::"
 );
 
 /*
  uncomment these console logs and write your if else statement and use them to console log out the correct statement
 */
 
-if (hasJetPack === true ) {
-  console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+if (hasJetPack === true) {
+    console.log("JetPack is enabled!!! off I go outside to fix the satellite");
 } else {
-  console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
+    console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 }
- 
+
 
 /*
  3. METEOR SHOWER::::::
@@ -146,28 +146,25 @@ if (hasJetPack === true ) {
       create an if condition in your loop to print only 1 response using a comparison operator.
 */
 console.log(
-  "\u{1F327} METEOR SHOWER::::::"
+    "\u{1F327} METEOR SHOWER::::::"
 );
 
 var responses = [
-  "Call basecamp and ask for assistance, but they are far away",
-  "Do nothing, just push through",
-  "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
-  "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
+    "Call basecamp and ask for assistance, but they are far away",
+    "Do nothing, just push through",
+    "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
+    "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
 ];
 
 
 // write your code here, use the console log beneath
 
-var nrOfResponses = responses.Length;
+var nrOfResponses = responses.length;
 
-for (var Nr = 0; Nr <= nrOfResponses; Nr++) {
-
-  if (responses[Nr] === 2) {
-
-    console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
- 
-  }
+for (var i = 0; i <= nrOfResponses; i++) {
+    if (responses[i] === responses[2]) {
+        console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+    } else {console.log("Ship has been kidnapped by aliens")}
 }
 
 
@@ -177,20 +174,30 @@ for (var Nr = 0; Nr <= nrOfResponses; Nr++) {
 /*
  4. ALIENS SPACE CRAFT ENCROACHING::::::
     Alien craft approaches its an enemy for sure...you need to shoot it down!!
-    a) convert the pseudoscope to real code
+    a) convert the pseudoscode to real code
        create the required variables below and fill in the correct values and dataTypes to make
        the if statements pass so that photonLaser fires pew pew pew.
     b)
 */
 console.log(
-  "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
+    "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
 );
 
 // a.
-var ___;
-var ___;
-var ___;
-var ___;
+var ship = "enemy";
+var ammo = 100;
+var photonlaser = true;
+//var ;
+
+if (ship==="enemy", photonlaser ===true)
+console.log("photonLaserEnabled");
+for (ammo = 0; ammo <= 100; ammo++) {
+   if(ammo <= 99) {
+    console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
+} else {
+        console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+    }
+}
 
 /*
   If its an enemyCraft && photonLaser && photonLaserEnabled
