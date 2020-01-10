2a2e804e1655efdfe0408efc58b54503c913c8f4
hani <salloumhani@gmail.com>
2020-01-10 10:15:31 +0100

    fixed branch

--- 1_space_odyssey/js/Hani-9000/app.js
+++ 1_space_odyssey/js/Hani-9000/app.js
@@ -8,7 +8,6 @@ ____/\\\\\\\\\_________/\\\\\\\_______/\\\\\\\\\_________/\\\\\\\__________
       ___/\\\/___________\//\\\____/\\\____/\\\/___________\//\\\____/\\\________
        __/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\_
         _\///////////////_____\///////_____\///////////////_____\///////_____\///__
-
 _____/\\\\\\\\\____________________/\\\\\\\\\\\_____/\\\\\\\\\\\\\________/\\\\\\\\\____________/\\\\\\\\\___/\\\\\\\\\\\\\\\_
  ___/\\\\\\\\\\\\\________________/\\\/////////\\\__\/\\\/////////\\\____/\\\\\\\\\\\\\_______/\\\////////___\/\\\///////////__
   __/\\\/////////\\\______________\//\\\______\///___\/\\\_______\/\\\___/\\\/////////\\\____/\\\/____________\/\\\_____________
@@ -27,7 +26,6 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
       __\///\\\__/\\\_____\/\\\_______/\\\_________\/\\\_________/\\\______\//\\\____/\\\______\//\\\___\/\\\____________________\/\\\_______
        ____\///\\\\\/______\/\\\\\\\\\\\\/__________\/\\\________\///\\\\\\\\\\\/____\///\\\\\\\\\\\/____\/\\\\\\\\\\\\\\\________\/\\\_______
         ______\/////________\////////////____________\///___________\///////////________\///////////______\///////////////_________\///________
-
       ============================================================================================
         TIMELINE of EVENTS
       ============================================================================================
@@ -35,27 +33,20 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
              Stone Age Earth:
              In the presence of a mysterious black obelisk, pre-humans discover the use
              of tools -- and weapons -- violently taking first steps toward intelligence.
-
         1999: On Earth's moon astronauts uncover another mysterious black obelisk.
-
         2001: Between Earth and Jupiter, the spacecraft's intelligent computer makes a mistake that
               kills most of the human crew -- then continues to kill to hide its error.
-
         2020: The sole survivor of the journey to Jupiter ascends to the next level of humanity.
  */
 
 /*
   You are the sole survivor who will make the journey to Jupiter.
-
   But before you can leave you must build an autonomous humanoid robot to help you on your mission.
-
   This humanoid will be used to perform dangerous and mission critical tasks on your journey to Jupiter.
   - space walks to repair the spaceship
   - fight off alien creatures
   -
-
   The humanoid is your doppelgänger, a sentient being that will help you save man from extinction by keeping you alive.
-
   Using JavaScript build the software that will allow us to describe and programme the humanoid to do our bidding.
   */
 
@@ -76,8 +67,6 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
          /-._/-._/
   ...    \   `\  \
           `-._/._/
-
-
  1. CREATE DROID::::::
     Below are all the variables needed to initialize an android into consciousness in its default state.
     a. assign the correct values to the variables below,
@@ -145,8 +134,10 @@ console.log(
   ];
   var response = responses.Length;
   for (var i = 0; i <= response; i++) {
-    console.log(response[i])
+  if(responses[i] === responses[2]) {
+    console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
   }
+}
   
   // write your code here, use the console log beneath
   // console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
@@ -166,10 +157,20 @@ console.log(
   );
   
   // a.
-  var enemyCraft = "encroaching";
-  var photonLaser = "enabled";
+  var enemyCraft = true;
+  var photonLaser = true;
   var photonLaserEnabled = true;
+  var bullets = 100;
 
+  if( enemyCraft && photonLaser && photonLaserEnabled){
+    if ( bullets <= 100) {
+   for(var i = 0; i < 100; i ++) {
+     console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
+    }
+  } else{
+      console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+      }
+    }
   
   
   /*
@@ -180,10 +181,3 @@ console.log(
           ELSE
             console.log("\u{1F9E8} DEAD \u{1F9E8} ");
   */
-
- for(var bullets = 0; bullets < 100; bullets ++) {
-   console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
-  }
-  if( bullets == 0){
-    console.log("\u{1F9E8} DEAD \u{1F9E8} ");
-  }
