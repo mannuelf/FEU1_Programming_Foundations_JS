7772a2d0533b17330ddeb2e644d4db985c7f7a13
Ben Noah <noahbenaas@gmail.com>
2020-01-10 13:24:15 +0100

    update the page

--- 1_space_odyssey/js/ben-9000/app.js
+++ 1_space_odyssey/js/ben-9000/app.js
@@ -39,7 +39,6 @@ console.log(
        cannot go outside, console log the correct log based on that condition. 
 */
 
-
 if (hasJetPack) {
   console.log("JetPack is enabled!!! off I go outside to fix the satellite");
 } else {
@@ -54,7 +53,7 @@ if (hasJetPack) {
     a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
       create an if condition in your loop to print only 1 response using a comparison operator.
 */
-3. METEOR SHOWER::::::
+/* 3. METEOR SHOWER::::::
 On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
 a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
   create an if condition in your loop to print only 1 response using a comparison operator.
@@ -62,16 +61,16 @@ a) Using a for loop iterate through the responses array and choose the appropria
 console.log("\u{1F327} METEOR SHOWER::::::");
 
 var responses = [
-"Call basecamp and ask for assistance, but they are far away",
-"Do nothing, just push through",
-"initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
-"ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
+  "Call basecamp and ask for assistance, but they are far away",
+  "Do nothing, just push through",
+  "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
+  "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
 ];
 
 for (var i = 0; i <= responses.length; i++) {
-if (responses[i] === responses[3]) {
-console.log(responses[i]);
-}
+  if (responses[i] === responses[2]) {
+    console.log(responses[i]);
+  }
 }
 
 // write your code here, use the console log beneath
@@ -92,21 +91,23 @@ var orderHasShipped = true;
 //honestly have no clue of what i did her
 
 // a.
-var enemyCraft = false;
+var enemyCraft = true;
 var photonLaser = true;
 var photonLaserEnabled = true;
 var bullets = 100;
 
+console.log("TEST",enemyCraft,photonLaser,photonLaserEnabled,bullets);
+
 if (enemyCraft && photonLaser && photonLaserEnabled) {
-if (bullets >= 100) {
-for (var i = 0; i <= 100; i++) {
-  console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
-}
+  if (bullets >= 100) {
+    for (var i = 0; i <= 100; i++) {
+      console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
+    }
+  } else {
+    console.log("\u{1F9E8} DEAD \u{1F9E8}");
+  }
 } else {
-console.log("\u{1F9E8} DEAD \u{1F9E8}");
-}
-} else {
-console.log("do your homework")
+  console.log("do your homework")
 }
 
 /*
