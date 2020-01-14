eef512f7b5060e1805d2db6b0e8945a2a432c043
henrikaag <55979072+henrikaag@users.noreply.github.com>
2020-01-14 09:58:07 +0100

    task 3
    didnt understand task 4 completely

--- 1_space_odyssey/js/henrik-9000/app.js
+++ 1_space_odyssey/js/henrik-9000/app.js
@@ -1,4 +1,4 @@
-/* ______   __  __   ______  ______   
+/* ______   __  __   ______  ______
         _..._
       .'     '.      _
      /    .-""-\   _/ \
@@ -19,7 +19,7 @@
 
  1. CREATE DROID::::::
     Below are all the variables needed to initialize an android into consciousness in its default state.
-    a. assign the correct values to the variables below, 
+    a. assign the correct values to the variables below,
     b. ensure to choose the correct values and datatypes
     c. the droid should be built in your likeness (a human, with human features, 2 arms, legs etc)
 */
@@ -45,22 +45,81 @@ console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, ha
 
 
 
-/* 
+/*
  2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
-    a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid 
-       cannot go outside, console log the correct log based on that condition. 
+    a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid
+       cannot go outside, console log the correct log based on that condition.
 */
 
 if (hasJetPack === true) {
     console.log("JetPack is enabled!!! off I go outside to fix the satellite");
-} else { 
+} else {
     console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 }
-/* 
+/*
  uncomment these console logs and write your if else statement and use them to console log out the correct statement
 
  console.log("JetPack is enabled!!! off I go outside to fix the satellite");
  console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 
  */
+
+
+ /*
+ 3. METEOR SHOWER::::::
+    On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
+    a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
+      create an if condition in your loop to print only 1 response using a comparison operator.
+*/
+console.log(
+  "\u{1F327} METEOR SHOWER::::::"
+);
+
+var responses = [
+  "Call basecamp and ask for assistance, but they are far away",
+  "Do nothing, just push through",
+  "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
+  "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
+];
+
+
+// write your code here, use the console log beneath
+// console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+
+var messages = responses.length;
+
+for (var i = 0; i <= responses.length; i++) {
+  if (responses[i] === "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger") {
+    console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+  }
+
+}
+
+
+/*
+ 4. ALIENS SPACE CRAFT ENCROACHING::::::
+    Alien craft approaches its an enemy for sure...you need to shoot it down!!
+    a) convert the pseudoscope to real code
+       create the required variables below and fill in the correct values and dataTypes to make
+       the if statements pass so that photonLaser fires pew pew pew.
+    b)
+*/
+console.log(
+  "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
+);
+
+// a.
+var ___;
+var ___;
+var ___;
+var ___;
+
+/*
+  If its an enemyCraft && photonLaser && photonLaserEnabled
+      IF bullets is greater than or equal to 100
+          shoot 100 photon-lazers by printing this log 100 times:
+          console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!"); (use a loop)
+        ELSE
+          console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+*/
