1debc9b8504cf4dca517cf0122505c0af56577ea
Victoria P <victoria.charlotte.pettersen@gmail.com>
2020-01-15 00:00:43 +0100

    Added assignment 5 text into master

--- 1_space_odyssey/js/vic-9000/app.js
+++ 1_space_odyssey/js/vic-9000/app.js
@@ -118,6 +118,7 @@ console.log(
   hasWeapon
 );
 
+
 /*
  2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
@@ -134,6 +135,7 @@ if (hasJetPack === true) {
   console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
 }
 
+
 /*
  3. METEOR SHOWER::::::
     On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
@@ -143,7 +145,6 @@ if (hasJetPack === true) {
 
 //1. create a for loop and iterate through the responses array, and choose the appropriate response to get out of danger
 //2. create an if condition in the loop and print 1 response using a comparison operator
-
 console.log(
   "\u{1F327} METEOR SHOWER::::::"
 );
@@ -164,6 +165,7 @@ for (var singleResponse = 0; singleResponse <= allResponses; singleResponse++) {
     break;
   }
 }
+
 
 /*
  4. ALIENS SPACE CRAFT ENCROACHING::::::
@@ -190,7 +192,6 @@ var ___;
         ELSE
           console.log("\u{1F9E8} DEAD \u{1F9E8} ");
 */
-
 console.log(
   "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
 );
@@ -209,3 +210,57 @@ if (enemyCraft && photonLaser && photonLaserEnabled === true) {
     console.log("\u{1F9E8} DEAD \u{1F9E8} ");
   }
 }
+
+
+/*
+ 5. LUNAR LANDING:::::
+    We got word of an extra terrestrial sighting on the moon... we have been tasked to land on the moon
+    to investigate it, we have aboard a NASA lunar module https://en.wikipedia.org/wiki/Apollo_Lunar_Module
+    This module will allow to land safely on the moon with the aid of the landing radar which takes inputs.
+
+    Ensure the correct co-ordinates and approach speed are entered into the landing radar.
+    It's all automated we just have to ensure that the correct values are input ie. not undefined.
+
+    The lunar radar accepts approach speeds in increments of hundreds only, starting at 0 and maxing out at 700.
+
+    You cannot edit the lunarRadar object directly you have to pass your values in via an interface called variables.
+
+    tip: varName.keyName (this is how to extract a value from an object)
+*/
+console.log(
+  "\u{1F311} LUNAR LANDING:::::"
+);
+
+// start coding here
+
+var lunarRadar = {
+  isEnabled: undefined,
+  long: undefined,
+  lat: undefined,
+  approachSpeed: undefined, // "ft per minute"
+}
+
+// Level 1 convert this psuedocode to functioning code.
+
+/*
+IF lunar radar is enabled && lat is defined and long is defined
+    console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+    IF approach speed is greater than or equal to 200ft per minute and approach speed is less than 299ft per minute
+      console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+    ELSE IF approach speed is greater than or equal to 300ft per minute
+      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 400ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 500ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 600ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is less than or equal to 100ft per minute
+      console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+    ELSE
+      console.log('Please enter an approach speed');
+ELSE
+  console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+*/
+
+// Level 2 convert it to a switch statement.
