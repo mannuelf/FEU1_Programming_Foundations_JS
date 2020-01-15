391a5be248f0db5b4e0e374ebbcb91f40d687d7c
mar-veloper <42553243+mar-veloper@users.noreply.github.com>
2020-01-15 11:46:18 +0100

    planetary_info-dash

--- 1_space_odyssey/js/jonmar-9000/app.js
+++ 1_space_odyssey/js/jonmar-9000/app.js
@@ -130,7 +130,7 @@ lunarRadar.approachSpeed = 500;
 //   console.log(
 //     "\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " +
 //       name +
-//       " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance."
+//       " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the Mass."
 //   );
 // }
 
@@ -182,6 +182,92 @@ if (
   console.log(
     "\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " +
       name +
-      " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance."
+      " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the Mass."
   );
 }
+
+// 6th  Exercise
+
+console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
+var planets = [
+  [
+    "MERCURY",
+    [
+      "The smallest and fastest planet, Mercury is the closest planet to the Sun and whips around it every 88 Earth days.",
+      "-173 to 427°C",
+      "330,104,000,000,000 billion kg (0.055 x Earth)"
+    ]
+  ],
+  [
+    "JUPITER",
+    [
+      "Jupiter is a massive planet, twice the size of all other planets combined and has a centuries-old storm that is bigger than Earth.",
+      "-108°C",
+      "1,898,130,000,000,000,000 billion kg (317.83 x Earth)"
+    ]
+  ],
+  [
+    "EARTH",
+    [
+      "Earth is the third planet from the Sun and the fifth largest planet in the Solar System with the highest density. It is currently the only known location where life is present.",
+      "110 degrees Fahrenheit / 48 degrees Celsius, and the lowest around -126 degrees Fahrenheit / -88 degrees Celsius, maybe even lower",
+      "6.6 sextillion tons"
+    ]
+  ],
+  [
+    "MARS",
+    [
+      "Mars is the fourth planet from the Sun and the second-smallest planet with a thin atmosphere, having the surface features reminiscent both of the impact craters of the Moon, and the valleys, deserts and polar ice caps of Earth. It is the most widely searched planet for life.",
+      "87 to -5 °C",
+      "641,693,000,000,000 billion kg (0.107 x Earth)"
+    ]
+  ]
+];
+
+console.group(planets[0][0]); // Mercury.
+console.log("Description:", planets[0][1][0]);
+console.log("Surface temperature:", planets[0][1][1]);
+console.log("Mass:", planets[0][1][2]);
+console.groupEnd();
+
+console.group(planets[1][0]); // Jupiter. (please insert the correct array index to add Earth as a string label to the console, as previous done for Mercury)
+console.log("Description:", planets[1][1][0]);
+console.log("Surface temperature:", planets[1][1][1]);
+console.log("Mass:", planets[1][1][2]);
+console.groupEnd();
+
+console.group(planets[2][0]); // Earth.
+console.log("Description:", planets[2][1][0]);
+console.log("Surface temperature:", planets[2][1][1]);
+console.log("Mass:", planets[2][1][2]);
+console.groupEnd();
+
+console.group(planets[3][0]); // Mars.
+console.log("Description:", planets[3][1][0]);
+console.log("Surface temperature:", planets[3][1][1]);
+console.log("Mass:", planets[3][1][2]);
+console.groupEnd();
+
+// Level 2::::::::::::::
+
+const neptune = [
+  "NEPTUNE",
+  [
+    "Neptune is the fourth largest and the farthest planet of the Solar System with the most powerful wind speeds out of all the planets. It is the smallest of the gas giants and is the first planet to be discovered by mathematical predictions in 1846.",
+    "−201 °C",
+    "102,410,000,000,000,000 billion kg (17.15x Earth)"
+  ]
+];
+const uranus = [
+  "URANUS",
+  [
+    "Uranus is the seventh planet discovered in the Solar System that also led to the discovery of the last planet, Neptune they are both referred to as ice giants. Officially recognized in 1781 after many observations in the past, it is the third largest planet of the Solar System.",
+    "−197 °C",
+    "86,810,300,000,000,000 billion kg (14.536 x Earth)"
+  ]
+];
+
+planets.unshift(neptune, uranus);
+
+console.table(planets);
