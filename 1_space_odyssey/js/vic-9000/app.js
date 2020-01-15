bdfb639ced9fa5d0e2b39b8a886275f1eb9f7830
Victoria P <victoria.charlotte.pettersen@gmail.com>
2020-01-15 17:11:51 +0100

    Done with assignment 5 and 6

--- 1_space_odyssey/js/vic-9000/app.js
+++ 1_space_odyssey/js/vic-9000/app.js
@@ -231,36 +231,200 @@ console.log(
   "\u{1F311} LUNAR LANDING:::::"
 );
 
-// start coding here
-
 var lunarRadar = {
   isEnabled: undefined,
   long: undefined,
   lat: undefined,
-  approachSpeed: undefined, // "ft per minute"
+  approachSpeed: undefined,
 }
 
-// Level 1 convert this psuedocode to functioning code.
+lunarRadar.isEnabled = true;
+lunarRadar.long = 1; //just adding something random
+lunarRadar.lat = 1; //just adding something random
+lunarRadar.approachSpeed = 200;
+
+// Level 1 v1:
+// using the code we are supposed to and not changing it
+// right now the code does not support speed 299 and 101-199, speed limit is infinite ("decrease pitch" outputs no matter the speed after 600)
+if (lunarRadar.isEnabled && lunarRadar.long !== undefined && lunarRadar.lat !== undefined) {
+  console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+  if (lunarRadar.approachSpeed >= 200 && lunarRadar.approachSpeed < 299) {
+    console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+  } else if (lunarRadar.approachSpeed >= 300) {
+    console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 400) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 500) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 600) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed <= 100) {
+    console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+  } else {
+    console.log('Please enter an approach speed');
+  }
+} else {
+  console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+}
 
 /*
-IF lunar radar is enabled && lat is defined and long is defined
-    console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
-    IF approach speed is greater than or equal to 200ft per minute and approach speed is less than 299ft per minute
-      console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
-    ELSE IF approach speed is greater than or equal to 300ft per minute
-      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
-    ELSE IF approach speed is greater than or equal to 400ft per minute
-      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
-    ELSE IF approach speed is greater than or equal to 500ft per minute
-      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
-    ELSE IF approach speed is greater than or equal to 600ft per minute
-      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
-    ELSE IF approach speed is less than or equal to 100ft per minute
-      console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
-    ELSE
-      console.log('Please enter an approach speed');
-ELSE
-  console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+// Level 1 v2:
+// editing the code adding support for "The lunar radar accepts approach speeds in increments of hundreds only, starting at 0 and maxing out at 700."
+// adding speed limit (after 700 else "you went down like a" executes)
+// making sure "Please enter an appropriate approach speed" executes if invalid speed input
+if (lunarRadar.isEnabled && lunarRadar.long !== undefined && lunarRadar.lat !== undefined) {
+  console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+  if (lunarRadar.approachSpeed === 100) { //speed 100
+    console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+  } else if (lunarRadar.approachSpeed === 200) { //speed 200
+    console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+  } else if (lunarRadar.approachSpeed === 300) { //speed 300
+    console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed === 400) { //speed 400
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed === 500) { //speed 500
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed === 600) { //speed 600
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed === 700) { //speed 700
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 701) { //speed over 701
+    console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+  } else { //invalid speed inout
+    console.log('Please enter an appropriate approach speed (100, 200, 300 or 400 etc.).');
+  }
+}
 */
 
-// Level 2 convert it to a switch statement.
+/*
+// Level 2:
+// editing code so it supports all speed limits and making sure default and "you went down like" execute at appropriate times
+console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+
+switch (true) {
+  case (lunarRadar.approachSpeed <= 199): //speed 0-199
+    console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+    break;
+  case (lunarRadar.approachSpeed >= 200): //speed 200-299
+    console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+    break;
+  case (lunarRadar.approachSpeed >= 300): //speed 300-399
+    console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+    break;
+  case (lunarRadar.approachSpeed >= 400): //speed 400-499
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    break;
+  case (lunarRadar.approachSpeed >= 500): //speed 500-599
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    break;
+  case (lunarRadar.approachSpeed >= 600): //speed 600-700
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    break;
+  case (lunarRadar.approachSpeed >= 701): //speed over 701
+    console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+    break;
+  default: //invalid input
+    console.log('Please enter an appropriate approach speed (100, 200, 300 or 400 etc.).');
+}
+*/
+
+
+/*
+ 6. PLANETARY INFO-DASH:::::
+ Level 1::::::::::::::
+ Given this array of planets using what you know about how to access data in an arrays index log out the:
+  - Description,
+  - Surface temperature and
+  - Mass
+  in the console logs bellow.
+  tip: pass your variables into the console.log after comma ,
+
+  Level 2::::::::::::::
+  Add 2 New planets of your choosing to the front of the Array : https://nineplanets.org
+*/
+// Level 1 and 2
+console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
+var planets = [
+  ["NEPTUNE",
+    [
+      "Neptune is the fourth largest and the farthest planet of the Solar System with the most powerful wind speeds out of all the planets. It is the smallest of the gas giants and is the first planet to be discovered by mathematical predictions in 1846.",
+      "-201°C",
+      "102,410,000,000,000,000 billion kg (17.15x Earth)"
+    ],
+  ],
+  ["VENUS",
+    [
+      "The second planet from the Sun and the sixth largest. Together with Mercury, they are the only planets without a satellite, even though Mercury is closer to the sun, Venus is the hottest planet.",
+      "462° C",
+      "4,867,320,000,000,000 billion kg (0.815 x Earth)"
+    ],
+  ],
+  ["MERCURY",
+    [
+      "The smallest and fastest planet, Mercury is the closest planet to the Sun and whips around it every 88 Earth days.",
+      "-173 to 427°C",
+      "330,104,000,000,000 billion kg (0.055 x Earth)"
+    ],
+  ],
+  ["JUPITER",
+    [
+      "Jupiter is a massive planet, twice the size of all other planets combined and has a centuries-old storm that is bigger than Earth.",
+      "-108°C",
+      "1,898,130,000,000,000,000 billion kg (317.83 x Earth)"
+    ],
+  ],
+  ["EARTH",
+    [
+      "Earth is the third planet from the Sun and the fifth largest planet in the Solar System with the highest density. It is currently the only known location where life is present.",
+      "110 degrees Fahrenheit / 48 degrees Celsius, and the lowest around -126 degrees Fahrenheit / -88 degrees Celsius, maybe even lower",
+      "6.6 sextillion tons"
+    ],
+  ],
+  ["MARS",
+    [
+      "Mars is the fourth planet from the Sun and the second-smallest planet with a thin atmosphere, having the surface features reminiscent both of the impact craters of the Moon, and the valleys, deserts and polar ice caps of Earth. It is the most widely searched planet for life.",
+      "87 to -5 °C",
+      "641,693,000,000,000 billion kg (0.107 x Earth)"
+    ],
+  ],
+];
+
+var allPlanets = planets.length;
+for (singlePlanet = 0; singlePlanet < allPlanets; singlePlanet++) {}
+
+console.group(planets[0][0]); // Neptune.
+console.log("Description:", planets[0][1][0]);
+console.log("Surface temperature:", planets[0][1][1]);
+console.log("Mass:", planets[0][1][2]);
+console.groupEnd();
+
+console.group(planets[1][0]); // Venus.
+console.log("Description:", planets[1][1][0]);
+console.log("Surface temperature:", planets[1][1][1]);
+console.log("Mass:", planets[1][1][2]);
+console.groupEnd();
+
+console.group(planets[2][0]); // Mercury.
+console.log("Description:", planets[2][1][0]);
+console.log("Surface temperature:", planets[2][1][1]);
+console.log("Mass:", planets[2][1][2]);
+console.groupEnd();
+
+console.group(planets[3][0]); // Jupiter.
+console.log("Description:", planets[3][1][0]);
+console.log("Surface temperature:", planets[3][1][1]);
+console.log("Mass:", planets[3][1][2]);
+console.groupEnd();
+
+console.group(planets[4][0]); // Earth.
+console.log("Description:", planets[4][1][0]);
+console.log("Surface temperature:", planets[4][1][1]);
+console.log("Mass:", planets[4][1][2]);
+console.groupEnd();
+
+console.group(planets[5][0]); // Mars.
+console.log("Description:", planets[5][1][0]);
+console.log("Surface temperature:", planets[5][1][1]);
+console.log("Mass:", planets[5][1][2]);
+console.groupEnd();
