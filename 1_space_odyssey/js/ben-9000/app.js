968e1b374fee4537e0e10ee8ac355230301bc079
Ben Noah <noahbenaas@gmail.com>
2020-01-16 21:47:42 +0100

    Todays assignment
    level2

--- 1_space_odyssey/js/ben-9000/app.js
+++ 1_space_odyssey/js/ben-9000/app.js
@@ -1,56 +1,112 @@
+/*
+ 7. Android Object REFACTOR:::::
+
+ Level 1::::::::::::::
+ Refactor (change) all the variables you have in place that initializes the android into an android object.
+ - Your android needs a head, upperBody, lowerBody & [ anyThingElseYouCanDreamOf ].
+ - Feel free to create any new properties that will further describe your android [ anyThingYouCanDreamOf ].
+ - next update your application to use this newly refactored code
+
+ tip: the console.log that prints the variables of the android will need to be updated
+
+ Level 2::::::::::::::
+ Refactor the planets array to be an array of objects, each object must be its own planet.
+  - next update your application to use this newly refactored code
+
+var isAlive = true;
+var name = "ben-9000";
+var hasJetPack = true;
+var suitColor = "black";
+var eyes = 4;
+var eyeColor = "brown";
+var hairColor = "black";
+var legs = 2;
+var arms = 2;
+var hands = 2;
+var ears = 2;
+var canWalk = true;
+var canRun = true;
+var canFly = true;
+var hasWeapon = false;
+var isInGoodMood = false;
+*/
+
+
 var android = {
-  head:{
-    name:"ben-9000",
-    eyes:2,
-    eyeColor:"brown",
-    hairColor:"black",
-    ears:2,
+  head: {
+    name: "ben-9000",
+    eyes: 2,
+    eyeColor: "brown",
+    hairColor: "black",
+    ears: 2
   },
-  upperBody:{
-    arms:2,
-    hands:2,
+  upperBody: {
+    arms: 2,
+    hands: 2
   },
-  lowerBody:{
-    legs:2,
+  lowerBody: {
+    legs: 2
   },
-  anyThingElseYouCanDreamOf:{
-    canWalk:true,
-    canRun:true,
-    canFly:true,
-    hasWeapon:false,
-    isInGoodMood:false,
-    isAlive:true,
-    suitColor:"black",
-    hasJetPack:true,
- },
+  anyThingElseYouCanDreamOf: {
+    canWalk: true,
+    canRun: true,
+    canFly: true,
+    hasWeapon: false,
+    isInGoodMood: false,
+    isAlive: true,
+    suitColor: "black",
+    hasJetPack: true
+  }
 };
 
 console.log(".-.-. Booting .-.-.");
 console.log(android.head.name);
+console.log(android.head.eyes);
+console.log(android.head.ears);
+console.log(android.head.eyeColor);
+console.log(android.head.hairColor);
 console.log(android.anyThingElseYouCanDreamOf.isAlive);
+console.log(android.anyThingElseYouCanDreamOf.canWalk);
+console.log(android.anyThingElseYouCanDreamOf.canFly);
+console.log(android.anyThingElseYouCanDreamOf.hasWeapon);
 console.log(android.anyThingElseYouCanDreamOf.hasJetPack);
+console.log(android.lowerBody.legs);
+console.log(android.upperBody.arms);
+console.log(android.upperBody.hands);
 
+//Refactor the planets array to be an array of objects, each object must be its own planet.
+//next update your application to use this newly refactored code
 
-
-
-
-
-/*
-  
- 
-  eyes,
-  eyeColor,
-  hairColor,
-  legs,
-  arms,
-  hands,
-  ears,
-  canWalk,
-  canFly,
-  hasWeapon
-);
-*/
-
+var planets = {              
+  Mercury: [ "MERCURY",
+  [
+    "The smallest and fastest planet, Mercury is the closest planet to the Sun and whips around it every 88 Earth days.",
+    "-173 to 427°C",
+    "330,104,000,000,000 billion kg (0.055 x Earth)"
+  ],
+], 
+  Jupiter:  [ "JUPITER",
+  [
+    "Jupiter is a massive planet, twice the size of all other planets combined and has a centuries-old storm that is bigger than Earth.",
+    "-108°C",
+    "1,898,130,000,000,000,000 billion kg (317.83 x Earth)"
+  ],
+],
+earth: [ "EARTH",
+[
+  "Earth is the third planet from the Sun and the fifth largest planet in the Solar System with the highest density. It is currently the only known location where life is present.",
+  "110 degrees Fahrenheit / 48 degrees Celsius, and the lowest around -126 degrees Fahrenheit / -88 degrees Celsius, maybe even lower",
+  "6.6 sextillion tons"
+],
+],
+mars: [ "MARS",
+[
+  "Mars is the fourth planet from the Sun and the second-smallest planet with a thin atmosphere, having the surface features reminiscent both of the impact craters of the Moon, and the valleys, deserts and polar ice caps of Earth. It is the most widely searched planet for life.",
+  "87 to -5 °C",
+  "641,693,000,000,000 billion kg (0.107 x Earth)"
+],
+]
+}; 
 
 /* 
  2. BROKEN SATELLITE::::::
@@ -58,6 +114,8 @@ console.log(android.anyThingElseYouCanDreamOf.hasJetPack);
     a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid 
        cannot go outside, console log the correct log based on that condition. 
 */
+
+hasJetPack = true;
 
 if (hasJetPack) {
   console.log("JetPack is enabled!!! off I go outside to fix the satellite");
@@ -116,7 +174,7 @@ var photonLaser = true;
 var photonLaserEnabled = true;
 var bullets = 100;
 
-console.log("TEST",enemyCraft,photonLaser,photonLaserEnabled,bullets);
+console.log("TEST", enemyCraft, photonLaser, photonLaserEnabled, bullets);
 
 if (enemyCraft && photonLaser && photonLaserEnabled) {
   if (bullets >= 100) {
@@ -127,7 +185,7 @@ if (enemyCraft && photonLaser && photonLaserEnabled) {
     console.log("\u{1F9E8} DEAD \u{1F9E8}");
   }
 } else {
-  console.log("do your homework")
+  console.log("do your homework");
 }
 
 /*
@@ -138,99 +196,5 @@ If its an enemyCraft && photonLaser && photonLaserEnabled
     ELSE
       console.log("\u{1F9E8} DEAD \u{1F9E8} ");
 */
-
-/*
- 7. Android Object REFACTOR:::::
-
- Level 1::::::::::::::
- Refactor (change) all the variables you have in place that initializes the android into an android object.
- - Your android needs a head, upperBody, lowerBody & [ anyThingElseYouCanDreamOf ].
- - Feel free to create any new properties that will further describe your android [ anyThingYouCanDreamOf ].
- - next update your application to use this newly refactored code
-
- tip: the console.log that prints the variables of the android will need to be updated
-
- Level 2::::::::::::::
- Refactor the planets array to be an array of objects, each object must be its own planet.
-  - next update your application to use this newly refactored code
-
-var isAlive = true;
-var name = "ben-9000";
-var hasJetPack = true;
-var suitColor = "black";
-var eyes = 4;
-var eyeColor = "brown";
-var hairColor = "black";
-var legs = 2;
-var arms = 2;
-var hands = 2;
-var ears = 2;
-var canWalk = true;
-var canRun = true;
-var canFly = true;
-var hasWeapon = false;
-var isInGoodMood = false;
-*/
-var android = {
-  head:{
-    name:"ben-9000",
-    eyes:2,
-    eyeColor:"brown",
-    hairColor:"black",
-    ears:2,
-  },
-  upperBody:{
-    arms:2,
-    hands:2,
-  },
-  lowerBody:{
-    legs:2,
-  },
-  anyThingElseYouCanDreamOf:{
-    canWalk:true,
-    canRun:true,
-    canFly:true,
-    hasWeapon:false,
-    isInGoodMood:false,
-    isAlive:true,
-    suitColor:"black",
- },
-};
-
-console.log(".-.-. Booting .-.-.");
-console.log(android.head);android
-
-
-
-console.log(
-  name,
-  isAlive,
-  hasJetPack,
-  eyes,
-  eyeColor,
-  hairColor,
-  legs,
-  arms,
-  hands,
-  ears,
-  canWalk,
-  canFly,
-  hasWeapon
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
