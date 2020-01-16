0de6733275f43ff16595b8b763363df1c1750b9d
cvampiri <cvampiri@gmail.com>
2020-01-16 09:57:46 +0100

    look at the planets
    they're great

--- 1_space_odyssey/js/luke-9000/app.js
+++ 1_space_odyssey/js/luke-9000/app.js
@@ -294,3 +294,112 @@ console.log(
           console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
     }
   
+
+
+/*
+ 6. PLANETARY INFO-DASH:::::
+
+ Level 1::::::::::::::
+ Given this array of planets using what you know about how to access data in an arrays index log out the:
+  - Description,
+  - Surface temperature and
+  - Mass
+
+  in the console logs bellow.
+
+  tip: pass your variables into the console.log after comma ,
+*/
+console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
+var planets = [
+  [ "MERCURY",
+    [
+      "The smallest and fastest planet, Mercury is the closest planet to the Sun and whips around it every 88 Earth days.",
+      "-173 to 427°C",
+      "330,104,000,000,000 billion kg (0.055 x Earth)"
+    ],
+  ],
+  [ "JUPITER",
+    [
+      "Jupiter is a massive planet, twice the size of all other planets combined and has a centuries-old storm that is bigger than Earth.",
+      "-108°C",
+      "1,898,130,000,000,000,000 billion kg (317.83 x Earth)"
+    ],
+  ],
+  [ "EARTH",
+    [
+      "Earth is the third planet from the Sun and the fifth largest planet in the Solar System with the highest density. It is currently the only known location where life is present.",
+      "110 degrees Fahrenheit / 48 degrees Celsius, and the lowest around -126 degrees Fahrenheit / -88 degrees Celsius, maybe even lower",
+      "6.6 sextillion tons"
+    ],
+  ],
+  [ "MARS",
+    [
+      "Mars is the fourth planet from the Sun and the second-smallest planet with a thin atmosphere, having the surface features reminiscent both of the impact craters of the Moon, and the valleys, deserts and polar ice caps of Earth. It is the most widely searched planet for life.",
+      "87 to -5 °C",
+      "641,693,000,000,000 billion kg (0.107 x Earth)"
+    ],
+  ],
+];
+planets.unshift( 
+  [ "HAUMEA",
+    [
+      "Haumea is the fastest rotating dwarf planet with the most interesting/controversial shape. It is located beyond the orbit of Neptune. It was discovered in 2004 and is the fourth largest dwarf planet.",
+      "-241°C",
+      "6,452,000,000 km (43.13 AU)"
+    ],
+  ],
+);
+
+planets.unshift(
+  [ "SATURN",
+    [
+      "Saturn is the sixth planet from the sun, with the largest planetary rings in the Solar System. It is the second-largest planet after Jupiter, and recently, with many other moons being discovered, it surpassed the number of Jupiter’s moons and is now considered the planet with the most numerous satellites.",
+      "-175°C",
+      "5.683 × 10^26 kg"
+    ],
+  ],
+);
+
+console.group(planets[0][0]); // Saturn.
+  console.log("Discription:", planets[0][1][0]);
+  console.log("Surface temperature:", planets[0][1][1]);
+  console.log("Distance:", planets[0][1][2]);
+console.groupEnd();
+
+console.group(planets[1][0]); // Haumea.
+  console.log("Description:", planets[1][1][0]);
+  console.log("Surface temperature:", planets[1][1][1]);
+  console.log("Distance:", planets[1][1][2]);
+console.groupEnd();
+
+console.group(planets[2][0]); // Mercury.
+  console.log("Description:", planets[2][1][0]);
+  console.log("Surface temperature:", planets[2][1][1]);
+  console.log("Distance:", planets[2][1][2]);
+console.groupEnd();
+
+console.group(planets[3][0]); // Jupiter. (please insert the correct array index to add Earth as a string label to the console, as previous done for Mercury)
+  console.log("Description:", planets[3][1][0]);
+  console.log("Surface temperature:", planets[3][1][1]);
+  console.log("Distance:", planets[3][1][2]);
+console.groupEnd();
+
+console.group(planets[4][0]); // Earth.
+  console.log("Description:", planets[4][1][0]);
+  console.log("Surface temperature:", planets[4][1][1]);
+  console.log("Distance:", planets[4][1][2]);
+console.groupEnd();
+
+console.group(planets[5][0]); // Mars.
+  console.log("Description:", planets[5][1][0]);
+  console.log("Surface temperature:", planets[5][1][1]);
+  console.log("Distance:", planets[5][1][2]);
+console.groupEnd();
+
+
+/*
+ Level 2::::::::::::::
+ Add 2 New planets of your choosing to the front of the Array : https://nineplanets.org
+*/
+
