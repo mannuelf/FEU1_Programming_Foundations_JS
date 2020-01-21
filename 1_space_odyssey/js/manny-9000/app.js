4c73b730bf9cb859a69ee71537c473a3b7e39995
Mannuel Ferreira <mannuel.ferreir@gmail.com>
2020-01-21 22:00:12 +0100

    functions functions functions

--- 1_space_odyssey/js/manny-9000/app.js
+++ 1_space_odyssey/js/manny-9000/app.js
@@ -1,245 +1,251 @@
-function bootUpManny() {
-  /*
-____/\\\\\\\\\_________/\\\\\\\_______/\\\\\\\\\_________/\\\\\\\__________
- __/\\\///////\\\_____/\\\/////\\\___/\\\///////\\\_____/\\\/////\\\________
-  _\///______\//\\\___/\\\____\//\\\_\///______\//\\\___/\\\____\//\\\_______
-   ___________/\\\/___\/\\\_____\/\\\___________/\\\/___\/\\\_____\/\\\_______
-    ________/\\\//_____\/\\\_____\/\\\________/\\\//_____\/\\\_____\/\\\__/\\\_
-     _____/\\\//________\/\\\_____\/\\\_____/\\\//________\/\\\_____\/\\\_\///__
-      ___/\\\/___________\//\\\____/\\\____/\\\/___________\//\\\____/\\\________
-       __/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\_
-        _\///////////////_____\///////_____\///////////////_____\///////_____\///__
+function bootUpManny9000() {
+  var isAlive = true;
+  var name = "Manny-9000";
+  var hasJetPack = undefined;
+  var suitColor = "Chrome";
+  var eyes = 2;
+  var eyeColor = "brown";
+  var hairColor = "black";
+  var legs = 2;
+  var arms = 2;
+  var hands = 2;
+  var ears = 2;
+  var canWalk = true;
+  var canRun = true;
+  var canFly = true;
+  var hasWeapon = true;
+  var isInGoodMood = true;
 
-_____/\\\\\\\\\____________________/\\\\\\\\\\\_____/\\\\\\\\\\\\\________/\\\\\\\\\____________/\\\\\\\\\___/\\\\\\\\\\\\\\\_
- ___/\\\\\\\\\\\\\________________/\\\/////////\\\__\/\\\/////////\\\____/\\\\\\\\\\\\\_______/\\\////////___\/\\\///////////__
-  __/\\\/////////\\\______________\//\\\______\///___\/\\\_______\/\\\___/\\\/////////\\\____/\\\/____________\/\\\_____________
-   _\/\\\_______\/\\\_______________\////\\\__________\/\\\\\\\\\\\\\/___\/\\\_______\/\\\___/\\\______________\/\\\\\\\\\\\_____
-    _\/\\\\\\\\\\\\\\\__________________\////\\\_______\/\\\/////////_____\/\\\\\\\\\\\\\\\__\/\\\______________\/\\\///////______
-     _\/\\\/////////\\\_____________________\////\\\____\/\\\______________\/\\\/////////\\\__\//\\\_____________\/\\\_____________
-      _\/\\\_______\/\\\______________/\\\______\//\\\___\/\\\______________\/\\\_______\/\\\___\///\\\___________\/\\\_____________
-       _\/\\\_______\/\\\_____________\///\\\\\\\\\\\/____\/\\\______________\/\\\_______\/\\\_____\////\\\\\\\\\__\/\\\\\\\\\\\\\\\_
-        _\///________\///________________\///////////______\///_______________\///________\///_________\/////////___\///////////////__
-_______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\________/\\\\\\\\\\\_____/\\\\\\\\\\\\\\\___/\\\________/\\\_
- _____/\\\///\\\_____\/\\\////////\\\___\///\\\____/\\\/_____/\\\/////////\\\____/\\\/////////\\\__\/\\\///////////___\///\\\____/\\\/__
-  ___/\\\/__\///\\\___\/\\\______\//\\\____\///\\\/\\\/______\//\\\______\///____\//\\\______\///___\/\\\________________\///\\\/\\\/____
-   __/\\\______\//\\\__\/\\\_______\/\\\______\///\\\/_________\////\\\____________\////\\\__________\/\\\\\\\\\\\__________\///\\\/______
-    _\/\\\_______\/\\\__\/\\\_______\/\\\________\/\\\_____________\////\\\____________\////\\\_______\/\\\///////_____________\/\\\_______
-     _\//\\\______/\\\___\/\\\_______\/\\\________\/\\\________________\////\\\____________\////\\\____\/\\\____________________\/\\\_______
-      __\///\\\__/\\\_____\/\\\_______/\\\_________\/\\\_________/\\\______\//\\\____/\\\______\//\\\___\/\\\____________________\/\\\_______
-       ____\///\\\\\/______\/\\\\\\\\\\\\/__________\/\\\________\///\\\\\\\\\\\/____\///\\\\\\\\\\\/____\/\\\\\\\\\\\\\\\________\/\\\_______
-        ______\/////________\////////////____________\///___________\///////////________\///////////______\///////////////_________\///________
-
-      ============================================================================================
-        TIMELINE of EVENTS
-      ============================================================================================
-       2001: A Space Odyssey spans from the dawn of man to its title year.
-             Stone Age Earth:
-             In the presence of a mysterious black obelisk, pre-humans discover the use
-             of tools -- and weapons -- violently taking first steps toward intelligence.
-
-        1999: On Earth's moon astronauts uncover another mysterious black obelisk.
-
-        2001: Between Earth and Jupiter, the spacecraft's intelligent computer makes a mistake that
-              kills most of the human crew -- then continues to kill to hide its error.
-
-        2020: The sole survivor of the journey to Jupiter ascends to the next level of humanity.
- */
-
-/*
-  You are the sole survivor who will make the journey to Jupiter.
-
-  But before you can leave you must build an autonomous humanoid robot to help you on your mission.
-
-  This humanoid will be used to perform dangerous and mission critical tasks on your journey to Jupiter.
-  - space walks to repair the spaceship
-  - fight off alien creatures
-  -
-
-  The humanoid is your doppelgänger, a sentient being that will help you save man from extinction by keeping you alive.
-
-  Using JavaScript build the software that will allow us to describe and programme the humanoid to do our bidding.
-  */
-
-/* ______   __  __   ______  ______
-        _..._
-      .'     '.      _
-     /    .-""-\   _/ \
-   .-|   /:.   |  |   |
-   |  \  |:.   /.-'-./
-   | .-'-;:__.'    =/
-   .'=  *=|Noroff_.='
-  /   _.  |    ;
- ;-.-'|    \   |
-/   | \    _\  _\
-\__/'._;.  ==' ==\
-         \    \   |
-         /    /   /
-         /-._/-._/
-  ...    \   `\  \
-          `-._/._/
-
-
- 1. CREATE DROID::::::
-    Below are all the variables needed to initialize an android into consciousness in its default state.
-    a. assign the correct values to the variables below,
-    b. ensure to choose the correct values and datatypes
-    c. the droid should be built in your likeness (a human, with human features, 2 arms, legs etc)
-*/
-var isAlive = true;
-var name = "Manny-9000";
-var hasJetPack = true;
-var suitColor = "Chrome";
-var eyes = 2;
-var eyeColor = "brown";
-var hairColor = "black";
-var legs = 2;
-var arms = 2;
-var hands = 2;
-var ears = 2;
-var canWalk = true;
-var canRun = true;
-var canFly = true;
-var hasWeapon = true;
-var isInGoodMood = true;
-
-console.log("\u{1F916} .-.-. Booting .-.-.");
-console.log(
-  name,
-  isAlive,
-  hasJetPack,
-  eyes,
-  eyeColor,
-  hairColor,
-  legs,
-  arms,
-  hands,
-  ears,
-  canWalk,
-  canFly,
-  hasWeapon,
-  isInGoodMood
-);
-
-/*
- 2. BROKEN SATELLITE::::::
-    You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
-    a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid
-       cannot go outside, console log the correct log based on that condition.
-*/
-console.log(
-  " \u{2747} BROKEN SATELLITE:::::: \u{2747} \u{2747} \u{2747} \u{2747}"
-);
-
-if (hasJetPack) {
-  console.log("JetPack is enabled!!! off I go outside to fix the satellite");
-} else {
+  console.log("\u{1F916} .-.-. Booting .-.-.");
   console.log(
-    "JetPack NOT enabled, please enable jetPack before doing a space walk"
+    name,
+    isAlive,
+    hasJetPack,
+    eyes,
+    eyeColor,
+    hairColor,
+    legs,
+    arms,
+    hands,
+    ears,
+    canWalk,
+    canFly,
+    hasWeapon,
+    isInGoodMood,
+    suitColor
   );
 }
 
-/*
- uncomment these console logs and write your if else statement and use them to console log out the correct statement
+function alienSpaceCraft() {
+  console.log("\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::");
 
- console.log("JetPack is enabled!!! off I go outside to fix the satellite");
- console.log("JetPack NOT enabled, please enable jetPack before doing a space walk");
- */
+  var enemyCraft = true;
+  var photonLaser = true;
+  var photonLaserEnabled = true;
+  var bullets = 100;
 
-/*
- 3. METEOR SHOWER::::::
-    On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
-    a. Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
-      create an if condition in your loop to print only 1 response using a comparison operator.
-*/
-console.log(
-  "\u{1F327} METEOR SHOWER::::::"
-);
-
-var responses = [
-  "Call basecamp and ask for assistance, but they are far away",
-  "Do nothing, just push through",
-  "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
-  "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
-];
-
-var numResponses = responses.length;
-for (var i = 0; i <= numResponses; i++) {
-  if (responses[i] === responses[2]) {
-    console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+  if (enemyCraft && photonLaser && photonLaserEnabled) {
+    if (bullets >= 100) {
+      bullets = i;
+      for (var i = 0; i < 100; i++) {
+        console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
+      }
+    } else {
+      console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+    }
   }
 }
 
-/*
-4. ALIENS SPACE CRAFT ENCROACHING::::::
-    Alien craft approaches its an enemy for sure...you need to shoot it down!!
-    a) create the required variables below and fill in the correct values and dataTypes to make
-       the if statements pass so that photonLaser fires pew pew pew.
-*/
-console.log(
-  "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
-);
+function lunarLanding() {
+  console.log("\u{1F311} LUNAR LANDING:::::");
 
-var enemyCraft = true;
-var photonLaser = true;
-var photonLaserEnabled = true;
-var bullets = 101;
+  var lunarRadar = {
+    isEnabled: undefined,
+    long: undefined,
+    lat: undefined,
+    approachSpeed: undefined // "ft per minute"
+  };
 
-if (enemyCraft && photonLaser && photonLaserEnabled) {
-  if (bullets >= 100) {
-    for(var i = 0; i <= 100; i++ ) {
-      console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
+  lunarRadar.approachSpeed = 240;
+  lunarRadar.lat = 20;
+  lunarRadar.long = 10;
+  lunarRadar.isEnabled = true;
+
+  if (
+    lunarRadar.isEnabled === true &&
+    lunarRadar.lat !== undefined &&
+    lunarRadar.long !== undefined
+  ) {
+    console.log(
+      "\u{1F91F} looking good " +
+        name +
+        " we are making our descent, now entering approach speed"
+    );
+    if (lunarRadar.approachSpeed >= 200 && lunarRadar.approachSpeed < 299) {
+      console.log(
+        "\u{269B} well done " +
+          name +
+          " we have touched down safely. Lets get some samples and get heck outta here!"
+      );
+    } else if (lunarRadar.approachSpeed >= 300) {
+      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+    } else if (lunarRadar.approachSpeed >= 400) {
+      console.log(
+        "\u{1F627} coming in way too hot " + name + " decrease pitch!"
+      );
+    } else if (lunarRadar.approachSpeed >= 500) {
+      console.log(
+        "\u{1F627} coming in way too hot " + name + " decrease pitch!"
+      );
+    } else if (lunarRadar.approachSpeed >= 600) {
+      console.log(
+        "\u{1F627} coming in way too hot " + name + " decrease pitch!"
+      );
+    } else if (lunarRadar.approachSpeed <= 100) {
+      console.log(
+        "\u{1F47D} hmmm need a bit more heat " +
+          name +
+          " lets increase pitch and we should have a smooth landing"
+      );
+    } else {
+      console.log(
+        "\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " +
+          name +
+          " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance."
+      );
     }
   } else {
-    console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+    console.log("Please enter an approach speed");
   }
 }
 
-/*
- 5. LUNAR LANDING:::::
-    We got word of an extra terrestrial sighting on the moon... we have been tasked to land on the moon
-    to investigate it, we have aboard a NASA lunar module https://en.wikipedia.org/wiki/Apollo_Lunar_Module
-    This module will allow to land safely on the moon with the aid of the landing radar which takes inputs.
-
-    Ensure the correct co-ordinates and approach speed are entered into the landing radar.
-    It's all automated we just have to ensure that the correct values are input ie. not undefined.
-
-    The lunar radar accepts approach speeds in increments of hundreds only, starting at 0 and maxing out at 700.
-
-    You cannot edit the lunarRadar object directly you have to pass your values in via an interface called variables.
-
-    tip: varName.keyName (this is how to extract a value from an object)
-*/
-console.log(
-  "\u{1F311} LUNAR LANDING:::::"
-);
-
-// start coding here
-
-var lunarRadar = {
-  long: undefined,
-  lat: undefined,
-  approachSpeed: undefined, // "ft per minute"
+function enableJetPack(pressed) {
+  var hasJetPack;
+  return (hasJetPack = pressed);
 }
 
-// Level 1 convert this psuedocode to functioning code.
+function spaceWalk(turnJetPackOn) { // pass in a function into this function
+  console.log(
+    " \u{2747} BROKEN SATELLITE:::::: \u{2747} \u{2747} \u{2747} \u{2747}"
+  );
 
-/*
-IF lat is defined and long is defined
-    console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
-    IF approach speed is greater than or equal to 200ft per minute and approach speed is less than 299ft per minute
-      console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
-    ELSE IF approach speed is greater than or equal to 300ft per minute
-      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
-    ELSE IF approach speed is greater than or equal to 600ft per minute
-      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
-    ELSE IF approach speed is less than or equal to 100ft per minute
-      console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
-    ELSE
-      console.log('Please enter an approach speed');
-ELSE
-  console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
-*/
+  if (turnJetPackOn) {
+    console.log("JetPack is enabled!!! off I go outside to fix the satellite");
+  } else {
+    console.log(
+      "JetPack NOT enabled, please enable jetPack before doing a space walk"
+    );
+  }
+}
 
-// Level 2 convert it to a switch statements.
+function meteorShower() {
+  console.log("\u{1F327} METEOR SHOWER::::::");
 
+  var responses = [
+    "Call basecamp and ask for assistance, but they are far away",
+    "Do nothing, just push through",
+    "initiate core thrusters, punch 180degrees into dash-nav and warp away from danger",
+    "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
+  ];
+
+  var numResponses = responses.length;
+  for (var i = 0; i <= numResponses; i++) {
+    if (responses[i] === responses[2]) {
+      console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+    }
+  }
+}
+
+function planetaryDashBoard() {
+  console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
+  var planets = [
+    [
+      "MERCURY",
+      [
+        "The smallest and fastest planet, Mercury is the closest planet to the Sun and whips around it every 88 Earth days.",
+        "-173 to 427°C",
+        "330,104,000,000,000 billion kg (0.055 x Earth)"
+      ]
+    ],
+    [
+      "JUPITER",
+      [
+        "Jupiter is a massive planet, twice the size of all other planets combined and has a centuries-old storm that is bigger than Earth.",
+        "-108°C",
+        "1,898,130,000,000,000,000 billion kg (317.83 x Earth)"
+      ]
+    ],
+    [
+      "EARTH",
+      [
+        "Earth is the third planet from the Sun and the fifth largest planet in the Solar System with the highest density. It is currently the only known location where life is present.",
+        "110 degrees Fahrenheit / 48 degrees Celsius, and the lowest around -126 degrees Fahrenheit / -88 degrees Celsius, maybe even lower",
+        "6.6 sextillion tons"
+      ]
+    ],
+    [
+      "MARS",
+      [
+        "Mars is the fourth planet from the Sun and the second-smallest planet with a thin atmosphere, having the surface features reminiscent both of the impact craters of the Moon, and the valleys, deserts and polar ice caps of Earth. It is the most widely searched planet for life.",
+        "87 to -5 °C",
+        "641,693,000,000,000 billion kg (0.107 x Earth)"
+      ]
+    ]
+  ];
+
+  planets.unshift(
+    [
+      "NEPTUNE",
+      [
+        "Neptune is the fourth largest and the farthest planet of the Solar System with the most powerful wind speeds out of all the planets. It is the smallest of the gas giants and is the first planet to be discovered by mathematical predictions in 1846.",
+        "-201°C",
+        "102,410,000,000,000,000 billion kg (17.15x Earth)"
+      ]
+    ],
+    [
+      "VENUS",
+      [
+        "The second planet from the Sun and the sixth largest. Together with Mercury, they are the only planets without a satellite, even though Mercury is closer to the sun, Venus is the hottest planet.",
+        "462° C",
+        "4,867,320,000,000,000 billion kg (0.815 x Earth)"
+      ]
+    ]
+  );
+
+  console.group(planets[0][0]); // Neptune.
+  console.log("Description:", planets[0][1][0]);
+  console.log("Surface temperature:", planets[0][1][1]);
+  console.log("Mass:", planets[0][1][2]);
+  console.groupEnd();
+
+  console.group(planets[1][0]); // Venus.
+  console.log("Description:", planets[1][1][0]);
+  console.log("Surface temperature:", planets[1][1][1]);
+  console.log("Mass:", planets[1][1][2]);
+  console.groupEnd();
+
+  console.group(planets[2][0]); // Mercury.
+  console.log("Description:", planets[2][1][0]);
+  console.log("Surface temperature:", planets[2][1][1]);
+  console.log("Mass:", planets[2][1][2]);
+  console.groupEnd();
+
+  console.group(planets[3][0]); // Jupiter.
+  console.log("Description:", planets[3][1][0]);
+  console.log("Surface temperature:", planets[3][1][1]);
+  console.log("Mass:", planets[3][1][2]);
+  console.groupEnd();
+
+  console.group(planets[4][0]); // Earth.
+  console.log("Description:", planets[4][1][0]);
+  console.log("Surface temperature:", planets[4][1][1]);
+  console.log("Mass:", planets[4][1][2]);
+  console.groupEnd();
+
+  console.group(planets[5][0]); // Mars.
+  console.log("Description:", planets[5][1][0]);
+  console.log("Surface temperature:", planets[5][1][1]);
+  console.log("Mass:", planets[5][1][2]);
+  console.groupEnd();
 }
