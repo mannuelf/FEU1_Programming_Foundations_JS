4533183816faa30782413245a546d2501b756608
Ben Noah <noahbenaas@gmail.com>
2020-01-21 17:18:59 +0100

    been cleaning up the code

--- 1_space_odyssey/js/ben-9000/app.js
+++ 1_space_odyssey/js/ben-9000/app.js
@@ -1,4 +1,120 @@
 /*
+____/\\\\\\\\\_________/\\\\\\\_______/\\\\\\\\\_________/\\\\\\\__________
+ __/\\\///////\\\_____/\\\/////\\\___/\\\///////\\\_____/\\\/////\\\________
+  _\///______\//\\\___/\\\____\//\\\_\///______\//\\\___/\\\____\//\\\_______
+   ___________/\\\/___\/\\\_____\/\\\___________/\\\/___\/\\\_____\/\\\_______
+    ________/\\\//_____\/\\\_____\/\\\________/\\\//_____\/\\\_____\/\\\__/\\\_
+     _____/\\\//________\/\\\_____\/\\\_____/\\\//________\/\\\_____\/\\\_\///__
+      ___/\\\/___________\//\\\____/\\\____/\\\/___________\//\\\____/\\\________
+       __/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\\\\\\\\\\\\\__\///\\\\\\\/____/\\\_
+        _\///////////////_____\///////_____\///////////////_____\///////_____\///__
+
+_____/\\\\\\\\\____________________/\\\\\\\\\\\_____/\\\\\\\\\\\\\________/\\\\\\\\\____________/\\\\\\\\\___/\\\\\\\\\\\\\\\_
+ ___/\\\\\\\\\\\\\________________/\\\/////////\\\__\/\\\/////////\\\____/\\\\\\\\\\\\\_______/\\\////////___\/\\\///////////__
+  __/\\\/////////\\\______________\//\\\______\///___\/\\\_______\/\\\___/\\\/////////\\\____/\\\/____________\/\\\_____________
+   _\/\\\_______\/\\\_______________\////\\\__________\/\\\\\\\\\\\\\/___\/\\\_______\/\\\___/\\\______________\/\\\\\\\\\\\_____
+    _\/\\\\\\\\\\\\\\\__________________\////\\\_______\/\\\/////////_____\/\\\\\\\\\\\\\\\__\/\\\______________\/\\\///////______
+     _\/\\\/////////\\\_____________________\////\\\____\/\\\______________\/\\\/////////\\\__\//\\\_____________\/\\\_____________
+      _\/\\\_______\/\\\______________/\\\______\//\\\___\/\\\______________\/\\\_______\/\\\___\///\\\___________\/\\\_____________
+       _\/\\\_______\/\\\_____________\///\\\\\\\\\\\/____\/\\\______________\/\\\_______\/\\\_____\////\\\\\\\\\__\/\\\\\\\\\\\\\\\_
+        _\///________\///________________\///////////______\///_______________\///________\///_________\/////////___\///////////////__
+_______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\________/\\\\\\\\\\\_____/\\\\\\\\\\\\\\\___/\\\________/\\\_
+ _____/\\\///\\\_____\/\\\////////\\\___\///\\\____/\\\/_____/\\\/////////\\\____/\\\/////////\\\__\/\\\///////////___\///\\\____/\\\/__
+  ___/\\\/__\///\\\___\/\\\______\//\\\____\///\\\/\\\/______\//\\\______\///____\//\\\______\///___\/\\\________________\///\\\/\\\/____
+   __/\\\______\//\\\__\/\\\_______\/\\\______\///\\\/_________\////\\\____________\////\\\__________\/\\\\\\\\\\\__________\///\\\/______
+    _\/\\\_______\/\\\__\/\\\_______\/\\\________\/\\\_____________\////\\\____________\////\\\_______\/\\\///////_____________\/\\\_______
+     _\//\\\______/\\\___\/\\\_______\/\\\________\/\\\________________\////\\\____________\////\\\____\/\\\____________________\/\\\_______
+      __\///\\\__/\\\_____\/\\\_______/\\\_________\/\\\_________/\\\______\//\\\____/\\\______\//\\\___\/\\\____________________\/\\\_______
+       ____\///\\\\\/______\/\\\\\\\\\\\\/__________\/\\\________\///\\\\\\\\\\\/____\///\\\\\\\\\\\/____\/\\\\\\\\\\\\\\\________\/\\\_______
+        ______\/////________\////////////____________\///___________\///////////________\///////////______\///////////////_________\///________
+
+      ============================================================================================
+        TIMELINE of EVENTS
+      ============================================================================================
+       2001: A Space Odyssey spans from the dawn of man to its title year.
+             Stone Age Earth:
+             In the presence of a mysterious black obelisk, pre-humans discover the use
+             of tools -- and weapons -- violently taking first steps toward intelligence.
+
+        1999: On Earth's moon astronauts uncover another mysterious black obelisk.
+
+        2001: Between Earth and Jupiter, the spacecraft's intelligent computer makes a mistake that
+              kills most of the human crew -- then continues to kill to hide its error.
+
+        2020: The sole survivor of the journey to Jupiter ascends to the next level of humanity.
+ */
+
+/*
+  You are the sole survivor who will make the journey to Jupiter.
+
+  But before you can leave you must build an autonomous humanoid robot to help you on your mission.
+
+  This humanoid will be used to perform dangerous and mission critical tasks on your journey to Jupiter.
+  - space walks to repair the spaceship
+  - fight off alien creatures
+  -
+
+  The humanoid is your doppelgänger, a sentient being that will help you save man from extinction by keeping you alive.
+
+  Using JavaScript build the software that will allow us to describe and programme the humanoid to do our bidding.
+  */
+
+/* ______   __  __   ______  ______
+        _..._
+      .'     '.      _
+     /    .-""-\   _/ \
+   .-|   /:.   |  |   |
+   |  \  |:.   /.-'-./
+   | .-'-;:__.'    =/
+   .'=  *=|Noroff_.='
+  /   _.  |    ;
+ ;-.-'|    \   |
+/   | \    _\  _\
+\__/'._;.  ==' ==\
+         \    \   |
+         /    /   /
+         /-._/-._/
+  ...    \   `\  \
+          `-._/._/
+
+
+<<<<<<< Updated upstream
+/*  8. REFACTOR Create functions:::::::
+  Level 1:::::::::
+  Refactor your application to be initialized by a function that will.
+
+  Level 2:::::::::
+  Look through your code and find more opportunities to use and call functions and parameters aka arguments.*/
+
+
+  
+=======
+ 1. CREATE DROID::::::
+    Below are all the variables needed to initialize an android into consciousness in its default state.
+    a. assign the correct values to the variables below,
+    b. ensure to choose the correct values and datatypes
+    c. the droid should be built in your likeness (a human, with human features, 2 arms, legs etc)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+>>>>>>> Stashed changes
+
+
+
+
+/*
  7. Android Object REFACTOR:::::
 
  Level 1::::::::::::::
@@ -9,9 +125,7 @@
 
  tip: the console.log that prints the variables of the android will need to be updated
 
- Level 2::::::::::::::
- Refactor the planets array to be an array of objects, each object must be its own planet.
-  - next update your application to use this newly refactored code
+
 
 var isAlive = true;
 var name = "ben-9000";
@@ -30,6 +144,7 @@ var canFly = true;
 var hasWeapon = false;
 var isInGoodMood = false;
 */
+
 
 var android = {
   head: {
@@ -73,8 +188,12 @@ console.log(android.lowerBody.legs);
 console.log(android.upperBody.arms);
 console.log(android.upperBody.hands);
 
+//Level 2::::::::::::::
 //Refactor the planets array to be an array of objects, each object must be its own planet.
-//next update your application to use this newly refactored code
+// - next update your application to use this newly refactored code
+
+
+
 
 var planet = [
   {
@@ -131,11 +250,6 @@ console.log(planet[i].mass);
 }
 
 
-
-
-
-
-
 /*console.group(planet[0].name); //Mercury
 console.log(planet[0].description);
 console.log(planet[0].mass);
@@ -152,6 +266,42 @@ console.group(planet[3].name); //Mars
 console.log(planet[3].description);
 console.log(planet[3].mass);
 */ 
+
+/*
+
+ 1. CREATE DROID::::::
+    Below are all the variables needed to initialize an android into consciousness in its default state.
+    a. assign the correct values to the variables below,
+    b. ensure to choose the correct values and datatypes
+    c. the droid should be built in your likeness (a human, with human features, 2 arms, legs etc)
+*/
+
+
+
+/*var isAlive = true;
+var name = "ben9000";
+var hasJetPack = true;
+var suitColor = "white";
+var eyes = 4;
+var eyeColor = "green";
+var hairColor = "svart";
+var legs = 3;
+var arms = 4;
+var hands = 4;
+var ears = 2;
+
+var canWalk = false;
+var canRun = false;
+var canFly = true;
+var hasWeapon = true;
+var isInGoodMood = true;
+
+
+console.log(".-.-. Booting .-.-.");
+console.log(name, isAlive, hasJetPack, eyes, eyeColor, hairColor, legs, arms, hands, ears, canWalk, canFly, hasWeapon);
+*/
+
+
 
 /* 
  2. BROKEN SATELLITE::::::
@@ -170,12 +320,6 @@ if (hasJetPack) {
   );
 }
 
-/*
- 3. METEOR SHOWER::::::
-    On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
-    a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
-      create an if condition in your loop to print only 1 response using a comparison operator.
-*/
 /* 3. METEOR SHOWER::::::
 On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
 a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
@@ -190,57 +334,169 @@ var responses = [
   "ask humanoid-9000 to fly out there and shield the ship from the debris, this action could harm the humanoid"
 ];
 
-for (var i = 0; i <= responses.length; i++) {
-  if (responses[i] === responses[2]) {
-    console.log(responses[i]);
-  }
-}
-
 // write your code here, use the console log beneath
 // console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
 
+for (var i = 0; i <= responses.length; i++) {
+  if (responses[i] === responses[2]) {
+  console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
+  }
+  }
+  
 /*
-4. ALIENS SPACE CRAFT ENCROACHING::::::
-Alien craft approaches its an enemy for sure...you need to shoot it down!!
-a) convert the pseudoscope to real code
-   create the required variables below and fill in the correct values and dataTypes to make
-   the if statements pass so that photonLaser fires pew pew pew.
-b)
+ 4. ALIENS SPACE CRAFT ENCROACHING::::::
+    Alien craft approaches its an enemy for sure...you need to shoot it down!!
+    a) convert the pseudoscope to real code
+       create the required variables below and fill in the correct values and dataTypes to make
+       the if statements pass so that photonLaser fires pew pew pew.
+    b)
 */
-console.log("\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::");
 
-var orderHasShipped = true;
+console.log(
+  "\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::"
+);
 
-//honestly have no clue of what i did her
 
-// a.
 var enemyCraft = true;
 var photonLaser = true;
 var photonLaserEnabled = true;
 var bullets = 100;
 
-console.log("TEST", enemyCraft, photonLaser, photonLaserEnabled, bullets);
-
 if (enemyCraft && photonLaser && photonLaserEnabled) {
   if (bullets >= 100) {
-    for (var i = 0; i <= 100; i++) {
+    bullets = i
+    for (var i = 0; i < 100; i++) {
       console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!");
     }
   } else {
-    console.log("\u{1F9E8} DEAD \u{1F9E8}");
+    console.log("\u{1F9E8} DEAD \u{1F9E8} ");
   }
-} else {
-  console.log("Oh no");
 }
 
+
 /*
-If its an enemyCraft && photonLaser && photonLaserEnabled
-  IF bullets is greater than or equal to 100
-      shoot 100 photon-lazers by printing this log 100 times:
-      console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!"); (use a loop)
-    ELSE
-      console.log("\u{1F9E8} DEAD \u{1F9E8} ");
+  If its an enemyCraft && photonLaser && photonLaserEnabled
+      IF bullets is greater than or equal to 100
+          shoot 100 photon-lazers by printing this log 100 times:
+          console.log("\u{2708} \u{1F525} pew pew pew !! you got them!!"); (use a loop)
+        ELSE
+          console.log("\u{1F9E8} DEAD \u{1F9E8} ");
 */
+
+
+/*
+ 5. LUNAR LANDING:::::
+    We got word of an extra terrestrial sighting on the moon... we have been tasked to land on the moon
+    to investigate it, we have aboard a NASA lunar module https://en.wikipedia.org/wiki/Apollo_Lunar_Module
+    This module will allow to land safely on the moon with the aid of the landing radar which takes inputs.
+
+    Ensure the correct co-ordinates and approach speed are entered into the landing radar.
+    It's all automated we just have to ensure that the correct values are input ie. not undefined.
+
+    The lunar radar accepts approach speeds in increments of hundreds only, starting at 0 and maxing out at 700.
+
+    You cannot edit the lunarRadar object directly you have to pass your values in via an interface called variables.
+
+    tip: varName.keyName (this is how to extract a value from an object)
+*/
+
+console.log(
+  "\u{1F311} LUNAR LANDING:::::"
+);
+
+// start coding here
+
+var lunarRadar = {
+  isEnabled: undefined,
+  long: undefined,
+  lat: undefined,
+  approachSpeed: undefined, // "ft per minute"
+}
+
+lunarRadar.approachSpeed = 240
+lunarRadar.lat = 20
+lunarRadar.long = 10
+lunarRadar.isEnabled = true
+
+
+if (lunarRadar.isEnabled === true && lunarRadar.lat !== undefined && lunarRadar.long !== undefined) {
+  console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+  if (lunarRadar.approachSpeed >= 200 && lunarRadar.approachSpeed < 299) {
+    console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+  } else if (lunarRadar.approachSpeed >= 300) {
+    console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 400) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 500) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed >= 600) {
+    console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+  } else if (lunarRadar.approachSpeed <= 100) {
+    console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+  } else {
+    console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+  }
+} else {
+  console.log('Please enter an approach speed');
+}
+
+// Level 1 convert this psuedocode to functioning code.
+
+/*
+IF lunar radar is enabled && lat is defined and long is defined
+    console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+    IF approach speed is greater than or equal to 200ft per minute and approach speed is less than 299ft per minute
+      console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+    ELSE IF approach speed is greater than or equal to 300ft per minute
+      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 400ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 500ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is greater than or equal to 600ft per minute
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+    ELSE IF approach speed is less than or equal to 100ft per minute
+      console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+    ELSE
+      console.log('Please enter an approach speed');
+ELSE
+  console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+*/
+
+
+// Level 2 convert it to a switch statement.
+
+if (lunarRadar.isEnabled === true && lunarRadar.lat !== undefined && lunarRadar.long !== undefined) {
+  console.log("\u{1F91F} looking good " + name + " we are making our descent, now entering approach speed");
+  switch (lunarRadar.approachSpeed) {
+    case 200:
+      console.log("\u{269B} well done " + name + " we have touched down safely. Lets get some samples and get heck outta here!");
+      break;
+    case 300:
+      console.log("\u{1F627} coming in hot " + name + " decrease pitch!");
+      break;
+    case 400:
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+      break;
+    case 500:
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+      break;
+    case 600:
+      console.log("\u{1F627} coming in way too hot " + name + " decrease pitch!");
+      break;
+    case 100:
+      console.log("\u{1F47D} hmmm need a bit more heat " + name + " lets increase pitch and we should have a smooth landing");
+      break;
+    default:
+      console.log("\u{1F9F1}\u{1F9F1}\u{1F9F1}\u{1F9F1} " + name + " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance.");
+  }
+} else {
+  console.log('Please enter an approach speed');
+}
+
+
+console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
 
 /*
  6. PLANETARY INFO-DASH:::::
@@ -255,6 +511,7 @@ If its an enemyCraft && photonLaser && photonLaserEnabled
 
   tip: pass your variables into the console.log after comma ,
 */
+
 console.log("\u{1F319} PLANETARY INFO-DASH:::::");
 
 var planets = [
@@ -291,8 +548,10 @@ var planets = [
     ]
   ]
 ];
-
-
+/*
+ Level 2::::::::::::::
+ Add 2 New planets of your choosing to the front of the Array : https://nineplanets.org
+*/
 var newPlanet = [
   "Neptunes",
   [
@@ -317,6 +576,7 @@ planets.unshift(newPlanet1);
 
 
 console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+
 
 console.group(planets[4][0]); // earth.done
   console.log("Description:", planets[4][1][0]);
@@ -359,11 +619,82 @@ console.groupEnd();
 
 
 
-/*
- Level 2::::::::::::::
- Add 2 New planets of your choosing to the front of the Array : https://nineplanets.org
-*/
 
 
-console.log(planets);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
