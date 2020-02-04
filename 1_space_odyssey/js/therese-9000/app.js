f876a0c19ca3b3b2787160c06d20ebe2026699b3
Therese Lybo <therese.lybo@gmail.com>
2020-02-04 13:26:21 +0100

    Making it clean

--- 1_space_odyssey/js/therese-9000/app.js
+++ 1_space_odyssey/js/therese-9000/app.js
@@ -1,4 +1,4 @@
-  /*
+/*
 ____/\\\\\\\\\_________/\\\\\\\_______/\\\\\\\\\_________/\\\\\\\__________        
  __/\\\///////\\\_____/\\\/////\\\___/\\\///////\\\_____/\\\/////\\\________       
   _\///______\//\\\___/\\\____\//\\\_\///______\//\\\___/\\\____\//\\\_______      
@@ -44,7 +44,7 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
         2020: The sole survivor of the journey to Jupiter ascends to the next level of humanity.
  */
 
-  /*
+/*
   You are the sole survivor who will make the journey to Jupiter.
 
   But before you can leave you must build an autonomous humanoid robot to help you on your mission.
@@ -59,7 +59,7 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
   Using JavaScript build the software that will allow us to describe and programme the humanoid to do our bidding.
   */
 
-  /* ______   __  __   ______  ______   
+/* ______   __  __   ______  ______   
         _..._
       .'     '.      _
      /    .-""-\   _/ \
@@ -86,7 +86,6 @@ _______/\\\\\________/\\\\\\\\\\\\______/\\\________/\\\______/\\\\\\\\\\\______
 */
 console.log("Please initialize Therese-9000");
 
-// function initialize()
 const initialize = () => {
   let android = [
     {
@@ -105,7 +104,7 @@ const initialize = () => {
       canRun: true,
       canFly: true,
       hasWeapon: true,
-      isInGoodMood: true,
+      isInGoodMood: true
     }
   ];
 
@@ -115,28 +114,16 @@ const initialize = () => {
   }
 
   return android;
-}
+};
 
-  /* 
+/* 
  2. BROKEN SATELLITE::::::
     You need to send the droid out on a space walk to fix the satellite, with out it you can't communicate with earth.
     a) write an if condition checking if the jet pack is enabled, if true the droid can go outside if false the droid 
        cannot go outside, console log the correct log based on that condition. 
 */
 
-// function fixSatellite()
-
-let jetPack = initialize()[0].hasJetPack
-
-// const fixSatellite = (getAndroid) => {
-//   if (getAndroid()[0].hasJetPack === true) {
-//     console.log("JetPack is enabled!!! off I go outside to fix the satellite");
-//   } else {
-//     console.log(
-//       "JetPack NOT enabled, please enable jetPack before doing a space walk"
-//     );
-//   }
-// }
+let jetPack = initialize()[0].hasJetPack;
 
 const fixSatellite = () => {
   if (jetPack === true) {
@@ -146,23 +133,19 @@ const fixSatellite = () => {
       "JetPack NOT enabled, please enable jetPack before doing a space walk"
     );
   }
-}
+};
 
-// function giveJetpackToAndroid(given)
-// const giveJetpackToAndroid = given => {
-//   return hasJetPack = given;
-// }
-
-const giveJetpackToAndroid = (given) => {
+const giveJetpackToAndroid = given => {
   jetPack = given;
-}
-  /*
+};
+/*
  3. METEOR SHOWER::::::
     On your way to Jupiter you encounter a massive meteor shower that could damage the ship, you need to redirect the ship to a new safe location.
     a) Using a for loop iterate through the responses array and choose the appropriate response to get out of danger.
       create an if condition in your loop to print only 1 response using a comparison operator.
 */
-  console.log("\u{1F327} METEOR SHOWER::::::");
+console.log("\u{1F327} METEOR SHOWER::::::");
+
 const meteorShower = () => {
   let responses = [
     "Call basecamp and ask for assistance, but they are far away",
@@ -178,9 +161,9 @@ const meteorShower = () => {
       console.log("\u{1F6F8} Swoosh -> -> Ship is steered to safety!");
     }
   }
-}
+};
 
-  /*
+/*
  4. ALIENS SPACE CRAFT ENCROACHING::::::
     Alien craft approaches its an enemy for sure...you need to shoot it down!!
     a) convert the pseudoscope to real code
@@ -188,9 +171,8 @@ const meteorShower = () => {
        the if statements pass so that photonLaser fires pew pew pew.
     b)
 */
-  console.log("\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::");
+console.log("\u{1F47E} ALIENS SPACE CRAFT ENCROACHING::::::");
 
-// function spacecraftEnroaching()
 const spacecraftEnroaching = () => {
   // a.
   let enemyCraft = true;
@@ -224,7 +206,7 @@ const spacecraftEnroaching = () => {
   }
 }
 
-  /*
+/*
  5. LUNAR LANDING:::::
     We got word of an extra terrestrial sighting on the moon... we have been tasked to land on the moon
     to investigate it, we have aboard a NASA lunar module https://en.wikipedia.org/wiki/Apollo_Lunar_Module
@@ -239,16 +221,15 @@ const spacecraftEnroaching = () => {
 
     tip: varName.keyName (this is how to extract a value from an object)
 */
-  console.log("\u{1F311} LUNAR LANDING:::::");
+console.log("\u{1F311} LUNAR LANDING:::::");
 
-// function lunarLanding()
 const lunarLanding = () => {
   let lunarRadar = {
     isEnabled: undefined,
     long: undefined,
     lat: undefined,
     approachSpeed: undefined // "ft per minute"
-  };
+  }
 
   lunarRadar.isEnabled = true;
   lunarRadar.long = 30.7723;
@@ -352,8 +333,8 @@ if(lunarRadar.isEnabled && lunarRadar.lat && lunarRadar.long){
         " you went down like a ton of bricks, the wrecked remains of the lunar lander can still be seen from earth twinkling in the distance."
     );
   }
-}
-  /*
+};
+/*
  6. PLANETARY INFO-DASH:::::
  Level 1::::::::::::::
  Given this array of planets using what you know about how to access data in an arrays index log out the:
@@ -365,9 +346,8 @@ if(lunarRadar.isEnabled && lunarRadar.lat && lunarRadar.long){
 
   tip: pass your variables into the console.log after comma ,
 */
-  console.log("\u{1F319} PLANETARY INFO-DASH:::::");
+console.log("\u{1F319} PLANETARY INFO-DASH:::::");
 
-// function planetInfo()
 const planetInfo = () => {
   let planets = [
     {
@@ -446,8 +426,8 @@ const planetInfo = () => {
     console.log("Mass:", planets[i].mass);
     console.groupEnd();
   }
-}
-  /*
+};
+/*
   8. REFACTOR Create functions:::::::
   Level 1:::::::::
   Refactor your application to be initialized by a function.
